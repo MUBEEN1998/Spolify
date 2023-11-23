98541ce4f8a3057b99184f850bc20b4514faffd4
MUBEEN1998 <mubeenmo1997@gmail.com>
2023-11-23 07:31:23 +0530

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express= require('express');
-const fs=require("fs");
 const server=express();
 
-fs.writeFileSync("first.txt","my name is mohd mubeen")
-fs.appendFileSync("first.txt"," i am the best of the world version")
-const data=fs.readFileSync("first.txt");
+
 server.get("/",(req,res)=>{
     res.send({name:"mohd mubeen"});
 })
